e31c0bc463c6ff9048f99f64c05a748af84df4c1
Weronika Slowinska <weronikaslowinska33@gmail.com>
2022-08-30 18:00:21 +0200

    Zad1ReverseString

--- Zad1ReverseString/MyStringStruct.cpp
+++ Zad1ReverseString/MyStringStruct.cpp
@@ -3,7 +3,7 @@
 std::string MyString::reverseString(std::string str)
 {
     std::stack<char> s;
-    for_each(str.begin(), str.end(), [&s](char sign) {s.push(sign); });
+    std::for_each(str.begin(), str.end(), [&s](char sign) {s.push(sign); });
     str.clear();
     while (!s.empty())
     {
