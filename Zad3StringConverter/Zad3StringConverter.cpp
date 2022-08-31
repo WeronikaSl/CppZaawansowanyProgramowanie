f3d8d412889bddef10a9baae728baa6eaba6c326
Weronika Slowinska <weronikaslowinska33@gmail.com>
2022-08-31 11:21:27 +0200

    Adding git ignore

--- Zad3StringConverter/Zad3StringConverter.cpp
+++ Zad3StringConverter/Zad3StringConverter.cpp
@@ -5,4 +5,6 @@ int main()
 {
     std::cout << StringConverter::toCamelCase("hello_s_d_a") << std::endl;
     std::cout << StringConverter::toSnakeCase("isThisFirstEntry") << std::endl;
+    std::cout << StringConverter::toCamelCase("get_colour") << std::endl;
+    std::cout << StringConverter::toSnakeCase("camelCase") << std::endl;
 }
