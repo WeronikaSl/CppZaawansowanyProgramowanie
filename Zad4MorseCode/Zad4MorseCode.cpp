86804a803e4eb0182290f94b3b3a33c978692fa2
Weronika Slowinska <weronikaslowinska33@gmail.com>
2022-09-03 09:39:16 +0200

    Zad4MorseCode

---
+++ Zad4MorseCode/Zad4MorseCode.cpp
@@ -0,0 +1,9 @@
+﻿#include "MorseCodeTranslator.hpp"
+#include <iostream>
+
+int main()
+{
+    MorseCodeTranslator mct;
+    std::cout << mct.fromMorseCode(".- -... -.-. -..") << std::endl;
+    std::cout << mct.toMorseCode("abcd") << std::endl;
+}
