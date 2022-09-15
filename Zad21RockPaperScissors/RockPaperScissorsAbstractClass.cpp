dccd4996e61339fd3cad4501d459d27a5b9fdbb5
Weronika Slowinska <weronikaslowinska33@gmail.com>
2022-09-15 16:01:20 +0200

    Zad21RockPaperScissors

--- Zad21RockPaperScissors/RockPaperScissorsAbstractClass.cpp
+++ Zad21RockPaperScissors/RockPaperScissorsAbstractClass.cpp
@@ -2,19 +2,19 @@
 
 int RockPaperScissorsAbstract::isRemis(RPS firstMove, RPS secondMove) const
 {
-	if (firstMove == RPS::PAPER && secondMove == RPS::PAPER) //osobna funkcja isRemis?
+	if (firstMove == RPS::PAPER && secondMove == RPS::PAPER)
 	{
-		std::cout << "Remis, nikt nie dostaje punktu" << std::endl; //zrobić funkcję
+		std::cout << "Remis, nikt nie dostaje punktu" << std::endl; 
 		return 0;
 	}
 	if (firstMove == RPS::ROCK && secondMove == RPS::ROCK)
 	{
-		std::cout << "Remis, nikt nie dostaje punktu" << std::endl; //zrobić funkcję
+		std::cout << "Remis, nikt nie dostaje punktu" << std::endl; 
 		return 0;
 	}
 	if (firstMove == RPS::SCISSORS && secondMove == RPS::SCISSORS)
 	{
-		std::cout << "Remis, nikt nie dostaje punktu" << std::endl; //zrobić funkcję
+		std::cout << "Remis, nikt nie dostaje punktu" << std::endl;
 		return 0;
 	}
 }
